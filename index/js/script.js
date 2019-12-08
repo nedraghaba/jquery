c49a3ff3947e6a8cf05652870d506f8e78c0cec9
nedraghaba <56513475+nedraghaba@users.noreply.github.com>
2019-12-08 15:14:30 +0100

    Add files via upload

---
+++ index/js/script.js
@@ -0,0 +1,27 @@
+$(".image-hover").mouseenter(function(){
+    $(this).find(".bt" ).show();
+    $(this).css("opacity","0.7");
+})
+$(".image-hover").mouseleave(function(){
+    $(this).find(".bt").hide();
+    $(this).css("opacity","1");
+
+})
+$(".image-hover").mouseenter(function(){
+    $(this).find(".bt2" ).show();
+    $(this).css("opacity","0.7");
+})
+$(".image-hover").mouseleave(function(){
+    $(this).find(".bt2").hide();
+    $(this).css("opacity","1");
+
+})
+$(".image-hover").mouseenter(function(){
+    $(this).find(".bt3" ).show();
+    $(this).css("opacity","0.7");
+})
+$(".image-hover").mouseleave(function(){
+    $(this).find(".bt3").hide();
+    $(this).css("opacity","1");
+
+})
